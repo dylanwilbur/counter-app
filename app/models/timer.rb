9eea663e9500071796293f08005efa7b4cffcb95
Dylan Wilbur <dylan.m.w@icloud.com>
2018-04-03 16:48:08 +0000

    Add unit tests to Timer model

--- app/models/timer.rb
+++ app/models/timer.rb
@@ -1,14 +1,16 @@
 class Timer < ApplicationRecord
   #validates :time, presence: true
+  before_create :elapsed_time
+  before_save :elapsed_time
   
   def initialize()
-    @time = Time.now
+    @start = Time.now
   end
   
   def elapsed_time
     now = Time.now
     elapsed = (now-@start).floor
-    this.time = elapsed
+    #self.time = elapsed
     elapsed
   end
 end
--- test/models/timer_test.rb
+++ test/models/timer_test.rb
@@ -5,4 +5,16 @@ class TimerTest < ActiveSupport::TestCase
     @now = Time.now
     @timer = Timer.new()
   end
+  
+  test "should be valid" do
+    assert @timer.valid?
+  end
+  
+  test "should get correct time" do
+    sleep(2)
+    correct_time = (Time.now - @now).floor
+    assert_equal(2, correct_time)
+    puts "correct time: " + correct_time.to_s
+    assert_equal(correct_time, @timer.elapsed_time)
+  end
 end
