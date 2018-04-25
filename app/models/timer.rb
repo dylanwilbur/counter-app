9112129f44b30b5fc8ded66232627cc2f6557c92
Dylan Wilbur <dylan.m.w@icloud.com>
2018-04-25 15:42:03 +0000

    Add remember methods to timer model

--- app/models/timer.rb
+++ app/models/timer.rb
@@ -1,4 +1,5 @@
 class Timer < ApplicationRecord
+  attr_accessor :remember_token
   #validates :time, presence: true
   #before_create :elapsed_time
   #before_save :elapsed_time
@@ -10,6 +11,20 @@ class Timer < ApplicationRecord
     else
       0
     end
+  end
+  
+  def Timer.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  def remember
+    self.remember_token = Timer.new_token
+    update_attribute(:remember_digest, Timer.digest(remember_token))
+  end
+  
+  # Returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   
 end
