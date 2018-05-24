cdfc58f17301b8f268f39e51ef5008c50161ff5f
Dylan Wilbur <dylan.m.w@icloud.com>
2018-05-24 00:42:39 +0000

    Add footer and begin frontend redesign

--- test/controllers/timers_controller_test.rb
+++ test/controllers/timers_controller_test.rb
@@ -1,9 +1,21 @@
 require 'test_helper'
 
 class TimersControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @timer = timers(:one)
+    @other_timer = timers(:two)
+  end
+  
   test "should get new" do
     get stopwatch_path
     assert_response :success
+  end
+  
+  test "should redirect show when logged in as wrong user" do
+    log_in_as(@other_timer)
+    get timer_path(@timer)
+    assert_redirected_to timer_path(@other_timer)
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,18 @@
+ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  
+  def is_logged_in?
+    !session[:timer_id].nil?
+  end
+  
+  def log_in_as(timer)
+    session[:timer_id] = timer.id
+  end
 
   # Add more helper methods to be used by all tests here...
 end
