58b9748fcb8425a8f69b6be82339274283153851
Dylan Wilbur <dylan.m.w@icloud.com>
2018-05-31 03:34:00 +0000

    Initialize story model and add preliminary model testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423154632) do
+ActiveRecord::Schema.define(version: 20180531032459) do
+
+  create_table "stories", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.string "img"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "timers", force: :cascade do |t|
     t.integer "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "remember_digest"
+    t.string "email"
   end
 
 end
